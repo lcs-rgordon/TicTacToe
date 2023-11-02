6cbb06056ac8bfbb8efaaf1bb701f3f2fd856437
Russell Gordon <rgordon@lcs.on.ca>
2023-11-02 17:14:29 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Russell Gordon on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
